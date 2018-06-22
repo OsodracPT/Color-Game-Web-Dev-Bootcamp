b7ec73d0d5c594878a271ba1b84cde3f4e69012a
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-06-22 09:35:03 +0100

    Added validation functions
    Added a random color generator in the javascript file

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,16 @@
 var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
+"rgb(255, 0, 0)",
+"rgb(255, 255, 0)",
+"rgb(0, 255, 0)",
+"rgb(0, 255, 255)",
+"rgb(0, 0, 255)",
+"rgb(255, 0, 255)"
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +24,24 @@ for(var i = 0; i < squares.length; i++){
 		var clickedColor = this.style.backgroundColor;
 		//compare color to pickedColor
 		if(clickedColor === pickedColor) {
-			alert("Correct!");
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
 		} else {
-			alert("WRONG!!!");
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try again";
 		}
 	});
+}
+
+function changeColors(color){
+	//loop through all the squares
+	for(var i = 0; i< squares.length; i++){
+		squares[i].style.backgroundColor = color;
+	}
+	//change all the color to match given color
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
 }
